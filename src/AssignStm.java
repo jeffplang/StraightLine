6e6f441539d9410541d30b657e12f3412222c291
Jeff Lang <jeffplang@gmail.com>
2017-09-08 08:23:28 -0400

    basic classes stubbed out

---
+++ src/AssignStm.java
@@ -0,0 +1,8 @@
+public class AssignStm {
+    public String id; public Exp exp;
+
+    public AssignStm(String i, Exp e) {
+        id = i;
+        exp = e;
+    }
+}
---
+++ src/EseqExp.java
@@ -0,0 +1,8 @@
+public class EseqExp extends Exp {
+    public Stm stm; public Exp exp;
+
+    public EseqExp(Stm s, Exp e) {
+        stm = s;
+        exp = e;
+    }
+}
